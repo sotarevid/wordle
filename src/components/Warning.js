3d90926df85829adb1ebf25a2a42cb78eae4a021
Igor Sotarev <igor@sotarev.ru>
2022-07-14 21:36:04 +0500

    fix Tailwind dynamic class

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Warning = ({ hidden }) => {
     return (
-        <span className={`py-4 px-8 bg-white rounded-lg border-2 border-dark self-center absolute translate-y-48 opacity-${hidden ? 0 : 100} z-10 transition-opacity duration-700`}>
+        <span className={`py-4 px-8 bg-white rounded-lg border-2 border-dark self-center absolute translate-y-48 ${hidden ? "opacity-0" : "opacity-100"} z-10 transition-opacity duration-700`}>
             This word is not in the words list!
         </span>
     )
