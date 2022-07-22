85254e08751f3fada8c476506f84255b91b0f6a0
Igor Sotarev <igor@sotarev.ru>
2022-07-22 14:34:46 +0500

    Add TypeScript support, rename js to ts | tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.js",
+        "./src/**/*.{html,js,ts,tsx}",
         "./public/index.html"
     ],
     theme: {
